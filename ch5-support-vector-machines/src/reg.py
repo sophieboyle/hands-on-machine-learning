e913e69fc99dca391110edf82fd1628e9ea7d98f
sophieboyle <sophieboyle4028@gmail.com>
2020-04-26 17:28:41 +0100

    Added example of usage of SVMs for regression

---
+++ ch5-support-vector-machines/src/reg.py
@@ -0,0 +1,21 @@
+from sklearn.svm import LinearSVR
+
+"""
+    @brief SVM can be used for regression as well.
+    This involves fitting as many instances as possible
+    within the margin (instead of outside, as with classification),
+    and minimising the number of instances outside the margin.
+    i.e. Margin violations in terms of SVM Regression regard
+    instances found outside the margin instead of inside.
+
+    NOTE: The Epsilon hyperparam determines the width
+    of the margin.
+
+    @param X matrice of feature values.
+    @param y array of labels.
+    @return Fitted SVM Regression model.
+"""
+def fit_svm_reg(X, y):
+    svm_reg = LinearSVR(epsilon=1.5)
+    svm_reg.fit(X, y)
+    return svm_reg
