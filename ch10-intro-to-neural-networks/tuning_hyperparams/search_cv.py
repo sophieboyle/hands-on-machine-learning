bb014be959ed02cba7b60b23e4fe64bffaffbf54
sophieboyle <sophieboyle4028@gmail.com>
2020-05-04 18:38:55 +0100

    Usage of scikitlearn wrapper on build-customised keras model

--- ch10-intro-to-neural-networks/tuning_hyperparams/search_cv.py
+++ ch10-intro-to-neural-networks/tuning_hyperparams/search_cv.py
@@ -1,5 +1,15 @@
 import tensorflow as tf
 from tensorflow import keras
+import sys
+import os
+import pathlib
+from scipy.stats import reciprocal
+from sklearn.model_selection import RandomizedSearchCV
+import numpy as np
+
+
+sys.path.append(os.path.join(pathlib.Path().absolute(), "sequential"))
+from cali_house_reg import get_housing
 
 
 def build_model(n_hidden=1, n_neurons=30, learning_rate=3e-3, input_shape=[8]):
@@ -31,3 +41,26 @@ def build_model(n_hidden=1, n_neurons=30, learning_rate=3e-3, input_shape=[8]):
     optimizer = keras.optimizers.SGD(lr=learning_rate)
     model.compile(loss="mse", optimizer=optimizer)
     return model
+
+
+if __name__ == "__main__":
+    X_train, X_val, X_test, y_train, y_val, y_test = get_housing()
+    X_new = X_test[:3]
+
+    # Thin wrapper around the keras model built using build model with
+    # default parameters.
+    keras_reg = keras.wrappers.scikit_learn.KerasRegressor(build_model)
+
+    # Therefore, methods can be used as if it were a scikit-learn reg.
+    keras_reg.fit(X_train, y_train, epochs=100, 
+                    validation_data=(X_val, y_val),
+                    callbacks=[keras.callbacks.EarlyStopping(patience=10)])
+
+    # NOTE: The score will be the opposite of MSE, as scikit-learn's
+    # score method works by providing higher values for better models.
+    mse_test = keras_reg.score(X_test, y_test)
+    print(f"SCORE: {mse_test}")
+
+    y_pred = keras_reg.predict(X_new)
+    print(y_pred)
+
