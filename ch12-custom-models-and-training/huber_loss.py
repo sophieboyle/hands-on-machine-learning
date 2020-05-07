ac7451f01713a095cdcec7390fb31f39f3507a07
sophieboyle <sophieboyle4028@gmail.com>
2020-05-07 23:00:53 +0100

    Added subclassing example for huber loss, also fixed mistake in parameterised huber loss

--- ch12-custom-models-and-training/huber_loss.py
+++ ch12-custom-models-and-training/huber_loss.py
@@ -27,7 +27,7 @@ def create_huber(threshold=1.0):
         is_small = tf.abs(error) < threshold
 
         squared_loss = tf.square(error) / 2
-        linear_loss = tf.abs(error) - 0.5
+        linear_loss = threshold * tf.abs(error) - threshold**2 / 2
 
         return tf.where(is_small, squared_loss, linear_loss)
     return huber_fn
@@ -46,3 +46,24 @@ def load_huber_model(filename, threshold):
     """
     return keras.models.load_model(filename, 
             custom_objects={"huber_fn" : create_huber(threshold)})
+
+
+class HuberLoss(keras.losses.Loss):
+    # By specifying HuberLoss as a class which subclasses keras'
+    # Loss class, it is possible to avoid having to hardcode the
+    # threshold when loading a saved model.
+    def __init__(self, threshold=1.0, **kwargs):
+        self.threshold = threshold
+    
+    def call(self, y_true, y_pred):
+        error = y_true - y_pred
+        is_small = tf.abs(error) < threshold
+
+        squared_loss = tf.square(error) / 2
+        linear_loss = threshold * tf.abs(error) - threshold**2 / 2
+
+        return tf.where(is_small, squared_loss, linear_loss)
+    
+    def get_config(self):
+        base_config = super().get_config()
+        return {**base_config, "threshold": self.threshold}
