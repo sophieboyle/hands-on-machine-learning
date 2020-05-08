d0a9a57625dd1944e413208a60daeb3f53be63d4
sophieboyle <sophieboyle4028@gmail.com>
2020-05-08 21:26:33 +0100

    Residual regressor custom model

--- ch12-custom-models-and-training/custom_models.py
+++ ch12-custom-models-and-training/custom_models.py
@@ -24,3 +24,22 @@ class ResidualBlock(keras.layers.Layer):
         return inputs + Z
 
 
+class ResidualRegressor(keras.Model):
+    """Residual Regressor custom model, which contains
+    a dense layer, 3 repetitions of the first residual
+    block, another residual block, and a final dense layer.
+    """
+    def __init__(self, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.hidden1 = keras.layers.Dense(30, activation="elu",
+                                    kernel_initializer="he_normal")
+        self.block1 = ResidualBlock(2, 30)
+        self.block2 = ResidualBlock(2, 30)
+        self.out = keras.layers.Dense(output_dim)
+    
+    def call(self, inputs):
+        Z = self.hidden1(inputs)
+        for _ in range(1 + 3):
+            Z = self.block1(Z)
+        Z = self.block2(Z)
+        return self.out(Z)
